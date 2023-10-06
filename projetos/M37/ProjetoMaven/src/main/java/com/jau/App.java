b7600b1123b1f7efc7625cd5d2874a952aa2ca85
JailsonJJS2 <jjs2@hotmail.com>
2023-10-06 15:37:39 -0300

    Concluido Exercicio M37

---
+++ projetos/M37/ProjetoMaven/src/main/java/com/jau/App.java
@@ -0,0 +1,13 @@
+package com.jau;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
